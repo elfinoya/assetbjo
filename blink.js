2d748c633888c5c7949467cabfb1789a65d13269
elfinoya <165668405+elfinoya@users.noreply.github.com>
2024-05-31 10:27:06 +0900

    Create blink.js

---
+++ blink.js
@@ -0,0 +1,6 @@
+<script type="text/javascript">
+            var blink = document.getElementById('blink');
+            setInterval(function() {
+                blink.style.opacity = (blink.style.opacity == 0 ? 1 : 0);
+            }, 1500);
+        </script>
