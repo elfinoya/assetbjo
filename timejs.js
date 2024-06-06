d4faeb84903636eb0b3ad459f49832a503f83e81
elfinoya <165668405+elfinoya@users.noreply.github.com>
2024-06-06 15:26:13 +0900

    Update timejs.js

--- timejs.js
+++ timejs.js
@@ -235,29 +235,29 @@ var x = setInterval(function() {
 }, 1000);
 <!--live-9-->
 // Set the date we're counting down to
-var countDownDate8 = new Date("June 06, 2024 20:00:00 GMT+07:00").getTime();
+var countDownDateA = new Date("June 06, 2024 20:00:00 GMT+07:00").getTime();
 
 // Update the count down every 1 second
 var x = setInterval(function() {
 
   // Get today's date and time
-  var now8 = new Date().getTime();
+  var nowA = new Date().getTime();
 
   // Find the distance between now and the count down date
-  var distance8 = countDownDate8 - now8;
+  var distanceA = countDownDateA - nowA;
 
   // Time calculations for days, hours, minutes and seconds
-  var days8 = Math.floor(distance8 / (1000 * 60 * 60 * 24));
-  var hours8 = Math.floor((distance8 % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-  var minutes8 = Math.floor((distance8 % (1000 * 60 * 60)) / (1000 * 60));
-  var seconds8 = Math.floor((distance8 % (1000 * 60)) / 1000);
+  var daysA = Math.floor(distanceA / (1000 * 60 * 60 * 24));
+  var hoursA = Math.floor((distanceA % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutesA = Math.floor((distanceA % (1000 * 60 * 60)) / (1000 * 60));
+  var secondsA = Math.floor((distanceA % (1000 * 60)) / 1000);
 
   // Display the result in the element with id="live7"
-  document.getElementById("live9").innerHTML = days8 + "d " + hours8 + "h "
-    + minutes8 + "m " + seconds8 + "s ";
+  document.getElementById("live9").innerHTML = daysA + "d " + hoursA + "h "
+    + minutesA + "m " + secondsA + "s ";
 
   // If the count down is finished, write some text
-  if (distance8 < 0) {
+  if (distanceA < 0) {
     clearInterval(x);
     document.getElementById("live9").innerHTML = "LIVE NOW 🔴";
   }
