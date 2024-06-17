0d31451120a8097df170ed9187ed64e71746dc66
elfinoya <165668405+elfinoya@users.noreply.github.com>
2024-06-17 15:43:28 +0900

    Update day1.js

--- day1.js
+++ day1.js
@@ -2,41 +2,26 @@
   */
 
 // Get today's current date.
-var date = new Date();
-var tahun = date.getFullYear();
-var bulan = date.getMonth();
-var tanggal = date.getDate();
-var hari = date.getDay();
-var jam = date.getHours();
-var menit = date.getMinutes();
-var detik = date.getSeconds();
-switch(hari) {
- case 0: hari = "Minggu"; break;
- case 1: hari = "Senin"; break;
- case 2: hari = "Selasa"; break;
- case 3: hari = "Rabu"; break;
- case 4: hari = "Kamis"; break;
- case 5: hari = "Jum'at"; break;
- case 6: hari = "Sabtu"; break;
-}
-switch(bulan) {
- case 0: bulan = "Januari"; break;
- case 1: bulan = "Februari"; break;
- case 2: bulan = "Maret"; break;
- case 3: bulan = "April"; break;
- case 4: bulan = "Mei"; break;
- case 5: bulan = "Juni"; break;
- case 6: bulan = "Juli"; break;
- case 7: bulan = "Agustus"; break;
- case 8: bulan = "September"; break;
- case 9: bulan = "Oktober"; break;
- case 10: bulan = "November"; break;
- case 11: bulan = "Desember"; break;
-}
-var tampilTanggal = "Tanggal: " + hari + ", " + tanggal + " " + bulan + " " + tahun;
-var tampilWaktu = "Jam: " + jam + ":" + menit + ":" + detik;
-console.log(tampilTanggal);
-console.log(tampilWaktu);
+<script type='text/javascript'>
 
-// Print out the data.
-document.write("" +today+ "");
+var months = ['Jan', 'Feb', 'Mar', 'Apr', 'Mei', 'Jun', 'Jul', 'Agst', 'Sept', 'Okt', 'Nov', 'Des'];
+
+var myDays = ['Minggu', 'Senin', 'Selasa', 'Rabu', 'Kamis', 'Jumat', 'Sabtu'];
+
+var date = new Date();
+
+var day = date.getDate();
+
+var month = date.getMonth();
+
+var thisDay = date.getDay(),
+
+    thisDay = myDays[thisDay];
+
+var yy = date.getYear();
+
+var year = (yy < 1000) ? yy + 1900 : yy;
+
+document.write(thisDay + ', ' + day + ' ' + months[month] + ' ' + year);
+
+</script>
