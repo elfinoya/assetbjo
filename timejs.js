4362cdb600ba1e421b75fb14d740f404c5f259e2
elfinoya <165668405+elfinoya@users.noreply.github.com>
2024-07-03 14:17:15 +0900

    Update timejs.js

--- timejs.js
+++ timejs.js
@@ -1,6 +1,6 @@
 <!--live-1-->
 // Set the date we're counting down to
-var countDownDate0 = new Date("Juli 02, 2024 23:00:00 GMT+07:00").getTime();
+var countDownDate0 = new Date("Juli 03, 2024 15:00:00 GMT+07:00").getTime();
 
 // Update the count down every 1 second
 var x = setInterval(function() {
