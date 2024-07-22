345c71e5d3845faeb2a8264b1ac534ae421c311f
elfinoya <165668405+elfinoya@users.noreply.github.com>
2024-07-22 15:46:31 +0900

    Update timejs.js

--- timejs.js
+++ timejs.js
@@ -1,6 +1,6 @@
 <!--live-1-->
 // Set the date we're counting down to
-var countDownDate0 = new Date("Juli 21, 2024 15:00:00 GMT+07:00").getTime();
+var countDownDate0 = new Date("Juli 22, 2024 15:00:00 GMT+07:00").getTime();
 
 // Update the count down every 1 second
 var x = setInterval(function() {
@@ -31,7 +31,7 @@ var x = setInterval(function() {
 }, 1000);
 <!--live-2-->
 // Set the date we're counting down to
-var countDownDate1 = new Date("JuLi 21, 2024 19:30:00 GMT+07:00").getTime();
+var countDownDate1 = new Date("JuLi 22, 2024 19:30:00 GMT+07:00").getTime();
 
 // Update the count down every 1 second
 var x = setInterval(function() {
@@ -60,7 +60,7 @@ var x = setInterval(function() {
 }, 1000);
 <!--live-3-->
 // Set the date we're counting down to
-var countDownDate2 = new Date("JuLi 19, 2024 15:30:00 GMT+07:00").getTime();
+var countDownDate2 = new Date("JuLi 22, 2024 15:30:00 GMT+07:00").getTime();
 
 // Update the count down every 1 second
 var x = setInterval(function() {
@@ -89,7 +89,7 @@ var x = setInterval(function() {
 }, 1000);
 <!--live-4-->
 // Set the date we're counting down to
-var countDownDate3 = new Date("JuLi 21, 2024 19:30:00 GMT+07:00").getTime();
+var countDownDate3 = new Date("JuLi 22, 2024 19:30:00 GMT+07:00").getTime();
 
 // Update the count down every 1 second
 var x = setInterval(function() {
