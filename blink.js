abd1c53a33fd08be14173847cf75d03418567e57
elfinoya <165668405+elfinoya@users.noreply.github.com>
2024-07-30 14:20:57 +0900

    Update blink.js

--- blink.js
+++ blink.js
@@ -1,7 +1,7 @@
 <script type="text/javascript">
 var blink_speed = 800; // every 1000 == 1 second, adjust to suit
 var t = setInterval(function () {
-var ele = document.getElementById('myBlinkingDiv');
+var ele = document.getElementById('blinking');
 ele.style.visibility = (ele.style.visibility == 'hidden' ? '' : 'hidden');
 }, blink_speed);
 </script>
