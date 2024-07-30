a96e7f979db2b957ac050760decc651489660b67
elfinoya <165668405+elfinoya@users.noreply.github.com>
2024-07-30 14:18:25 +0900

    Update blink.js

--- blink.js
+++ blink.js
@@ -1,6 +1,6 @@
 <script type="text/javascript">
-            var blink = document.getElementById('blink');
+            var blink = document.getElementById('blinking');
             setInterval(function() {
                 blink.style.opacity = (blink.style.opacity == 0 ? 1 : 0);
-            }, 1500);
+            }, 800);
         </script>
